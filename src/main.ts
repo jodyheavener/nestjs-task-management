5275398a632c58afda880fe44aca35d9b090c610
Jody Heavener <j.heavener@gmail.com>
2020-11-15 22:29:59 -0500

    Add prod cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const serverConfig = config.get('server');
   const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+  }
+
   await app.listen(port);
 }
 bootstrap();
